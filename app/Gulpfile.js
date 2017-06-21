2f96e233b3a701fda159d14b0c88bd83c8b2c2c4
Curt Grimes <github@curtgrimes.com>
2017-06-21 02:41:56 -0500

    Add Bootstrap and fixed bottom navbar

--- app/Gulpfile.js
+++ app/Gulpfile.js
@@ -4,15 +4,21 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var merge = require('gulp-merge');
 var cleanCSS = require('gulp-clean-css');
+var sass = require('gulp-sass');
 
 gulp.task('default', function () {
-    var jsStream = gulp.src('js/*')
+    var jsStream = gulp.src([
+            'js/*',
+            'node_modules/bootstrap/js/dist/collapse.js',
+            'node_modules/bootstrap/js/dist/util.js'
+        ])
         .pipe(concat('build.js'))
         .pipe(uglify())
         .pipe(gulp.dest('build/js'));
     
-    var cssStream = gulp.src('css/*')
-        .pipe(concat('build.css'))
+    var cssStream = gulp.src('scss/*')
+        .pipe(concat('build.scss'))
+        .pipe(sass().on('error', sass.logError))
         .pipe(cleanCSS())
         .pipe(gulp.dest('build/css'));
 
