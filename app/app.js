83c1d03c58df7d9190ff1106cebf97aca40a81db
Curt Grimes <github@curtgrimes.com>
2017-06-23 01:23:38 -0500

    Add favicons. Closes #12.

--- app/app.js
+++ app/app.js
@@ -14,13 +14,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'static/favicon')));
 app.use(express.static(path.join(__dirname, 'build')));
+app.use('/static/img', express.static(path.join(__dirname, 'static/img')));
 
 app.use('/', index);
 app.use('/users', users);
