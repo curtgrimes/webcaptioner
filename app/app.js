131a17deb0dab92814646b4ec8b86744aa29f141
Curt Grimes <github@curtgrimes.com>
2017-06-29 02:23:58 -0500

    Enable gzip compression

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var compression = require('compression');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -19,6 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(compression())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
