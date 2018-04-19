9da9241932fa3b29a9e9ff956043e5bb6c114686
Curt Grimes <github@curtgrimes.com>
2018-04-19 15:12:19 -0500

    Remove hardcoded localhost path

--- app/src/components/ChromelessWindowManager.js
+++ app/src/components/ChromelessWindowManager.js
@@ -1,5 +1,5 @@
 const start = function start(RemoteEventBus) {
-    let chromelessWindow = window.open('http://localhost:8080/receivers/chromeless', 'WebCaptionerChild', 'height=700,width=900,toolbar=0,location=0,menubar=0');
+    let chromelessWindow = window.open('/receivers/chromeless', 'WebCaptionerChild', 'height=700,width=900,toolbar=0,location=0,menubar=0');
 
     RemoteEventBus.$on('sendMutationToReceivers', ({type, payload}) => {
         chromelessWindow.dispatchEvent(new CustomEvent('processMessage', {detail: {type, payload}}));
