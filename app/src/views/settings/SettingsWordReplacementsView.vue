4f6d124deb4b40fb05d0ec7f58df34e9d4a6924f
Curt Grimes <github@curtgrimes.com>
2018-06-23 23:57:09 -0500

    Fix empty word replacements being persisted incorrectly

--- app/src/views/settings/SettingsWordReplacementsView.vue
+++ app/src/views/settings/SettingsWordReplacementsView.vue
@@ -2,24 +2,24 @@
   <div class="settings-word-replacements-view">
     <p>Replace or hide specific words or phrases during captioning.</p>
     <div class="row">
-      <div class="col-5">
+      <div :class="shouldShowDeleteButtons ? 'col-5' : 'col-6'">
         <div class="mb-1 font-weight-bold">Word or Phrase to Replace</div>
         <p class="small">Separate multiple words or phrases with commas. Not case sensitive.</p>
       </div>
-      <div class="col-5">
+      <div :class="shouldShowDeleteButtons ? 'col-5' : 'col-6'">
         <div class="mb-1 font-weight-bold">Replace With</div>
       </div>
     </div>
     <div ref="wordReplacements">
       <div class="row mb-2" v-for="(wordReplacement, index) in wordReplacements" v-bind:key="index">
-        <div class="col-5">
+        <div :class="shouldShowDeleteButtons ? 'col-5' : 'col-6'">
           <input type="text" v-model="wordReplacement.from" @keyup="updateWordReplacement(wordReplacement, index)" class="form-control" placeholder="Word or phrase to replace" v-bind:ref="index == wordReplacements.length - 1 ? 'lastWordReplacement' : ''"  />
         </div>
-        <div class="col-5">
+        <div :class="shouldShowDeleteButtons ? 'col-5' : 'col-6'">
           <input type="text" v-model="wordReplacement.to" @keyup="updateWordReplacement(wordReplacement, index)" class="form-control" placeholder="Replace with" />
         </div>
-        <div class="col-1">
-          <button type="button" v-if="wordReplacements.length > 1" @click="removeWordReplacement(index)" class="btn btn-danger btn-sm" data-toggle="tooltip" data-animation="false" data-placement="top" title="Remove"><fa icon="minus-circle" /></button>
+        <div class="col-1" v-if="shouldShowDeleteButtons">
+          <button type="button" @click="removeWordReplacement(index)" class="btn btn-danger btn-sm" data-toggle="tooltip" data-animation="false" data-placement="top" title="Remove"><fa icon="minus-circle" /></button>
         </div>
       </div>
     </div>
@@ -33,18 +33,25 @@ import debounce from 'lodash.debounce'
 export default {
   name: 'settings-word-replacements-view',
   mounted: function () {
-    if (!this.wordReplacements.length) {
-      console.log('adding a word replacement');
-      this.addWordReplacement({from:'', to: ''});
-    }
+    let self = this;
+    setTimeout(() => {
+      console.log('here');
+      if (!self.wordReplacements.length) {
+        self.addWordReplacement({from:'', to: ''});
+      }
+    },200); // hack
+  },
+  beforeDestroy: function() {
+    this.removeEmptyReplacements();
   },
   computed: {
     wordReplacements: {
       get () {
-        return this.$store.state.settings.wordReplacements.sort((r1, r2) => {
-          return r1.from < r2.from;
-        });
+        return this.$store.state.settings.wordReplacements;
       },
+    },
+    shouldShowDeleteButtons: function() {
+      return this.wordReplacements.length > 1;
     },
   },
 
@@ -64,6 +71,13 @@ export default {
     updateWordReplacement: debounce(function (wordReplacement, index) {
       this.$store.commit('UPDATE_WORD_REPLACEMENT', { wordReplacement, index });
     }, 200),
+    removeEmptyReplacements: function() {
+      this.wordReplacements.forEach((replacement, index) => {
+        if (replacement.from === '') {
+          this.removeWordReplacement(index);
+        }
+      });
+    },
   },
 }
 </script>
