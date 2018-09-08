525424e59a126585b5dfdff22004a6515989558c
Curt Grimes <github@curtgrimes.com>
2018-09-08 10:00:06 -0500

    Var test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
+
 # Get env variables for this deployment environment
 # https://gitlab.com/gitlab-org/gitlab-ce/issues/20367#note_89285011
-ENV_VAR_SUFFIX="__${CI_ENVIRONMENT_SLUG}"
 for var in $(env | awk -F "=" '{print $1}' | grep "${ENV_VAR_SUFFIX}$")
 do
     export ${var%${ENV_VAR_SUFFIX}}="${!var}"
