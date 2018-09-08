b77b9116c03e7c7d2a69b052f85425da0d121449
Curt Grimes <github@curtgrimes.com>
2018-09-08 00:50:59 -0500

    Var test

--- scripts/init-env.sh
+++ scripts/init-env.sh
@@ -1,8 +1,12 @@
-#!/usr/bin/env bash
 ENV_VAR_SUFFIX="__${CI_ENVIRONMENT_SLUG}"
+echo "hello script"
+echo $ENV_VAR_SUFFIX
+env | awk -F "=" '{print $1}' | grep "${ENV_VAR_SUFFIX}$"
 
 for var in $(env | awk -F "=" '{print $1}' | grep "${ENV_VAR_SUFFIX}$")
 do
+    echo "var"
     echo ${var%${ENV_VAR_SUFFIX}}
     export ${var%${ENV_VAR_SUFFIX}}="${!var}"
 done 
+
