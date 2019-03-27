132c33eb7428589543d0cd70a3e2a73671a535ef
Curt Grimes <github@curtgrimes.com>
2019-03-27 14:58:56 +0000

    Update testing step in gitlab ci

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 docker pull $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_NAME
 
-docker run -it $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_NAME /bin/bash
+docker run -i $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_NAME /bin/bash
 
 cd app
 npm run tests
