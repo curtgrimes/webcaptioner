142c679a0b78f578195668a97e03c3a6ef4fcafc
Curt Grimes <github@curtgrimes.com>
2019-07-28 22:11:30 -0500

    Add porn/p*** to uncensor list

--- app/mixins/data/profanity-uncensor-en.js
+++ app/mixins/data/profanity-uncensor-en.js
@@ -1,50 +1,53 @@
-export default [
-    {
-        from: 'f***',
-        to: 'fuck',
-    },
-    {
-        from: 'f******',
-        to: 'fucking',
-    },
-    {
-        from: 'f*****',
-        to: 'fucked',
-    },
-    {
-        from: 'f*******',
-        to: 'fuckfest',
-    },
-    {
-        from: 's***',
-        to: 'shit', // "slut" conflicts
-    },
-    {
-        from: 's*****',
-        to: 'slutty',
-    },
-    {
-        from: 'a******',
-        to: 'asshole',
-    },
-    {
-        from: 'c***',
-        to: 'cunt',
-    },
-    {
-        from: 'b****',
-        to: 'bitch',
-    },
-    {
-        from: 'b******',
-        to: 'bitches',
-    },
-    {
-        from: 'c**********',
-        to: 'cocksucker',
-    },
-    {
-        from: 'm***',
-        to: 'milf',
-    },
+export default [{
+    from: 'f***',
+    to: 'fuck',
+  },
+  {
+    from: 'f******',
+    to: 'fucking',
+  },
+  {
+    from: 'f*****',
+    to: 'fucked',
+  },
+  {
+    from: 'f*******',
+    to: 'fuckfest',
+  },
+  {
+    from: 's***',
+    to: 'shit', // "slut" conflicts
+  },
+  {
+    from: 's*****',
+    to: 'slutty',
+  },
+  {
+    from: 'a******',
+    to: 'asshole',
+  },
+  {
+    from: 'c***',
+    to: 'cunt',
+  },
+  {
+    from: 'b****',
+    to: 'bitch',
+  },
+  {
+    from: 'b******',
+    to: 'bitches',
+  },
+  {
+    from: 'c**********',
+    to: 'cocksucker',
+  },
+  {
+    from: 'm***',
+    to: 'milf',
+  },
+  {
+    from: 'p***',
+    to: 'porn',
+  },
 ];
