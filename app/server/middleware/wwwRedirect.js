122bc7b27adcf75943398a7162ff324b9ca7d18f
Curt Grimes <github@curtgrimes.com>
2020-01-09 23:51:01 -0600

    Add protocol to www location redirect

--- app/server/middleware/wwwRedirect.js
+++ app/server/middleware/wwwRedirect.js
@@ -2,7 +2,9 @@ module.exports = function(req, res, next) {
   const host = req.headers.host;
 
   if (process.env.NODE_ENV === 'production' && host.includes('www.')) {
-    res.writeHead(301, { Location: host.replace('www.', '') + req.url });
+    res.writeHead(301, {
+      Location: 'https://' + host.replace('www.', '') + req.url,
+    });
     return res.end();
   }
 
