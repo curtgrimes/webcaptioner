7e72d83f862e05c9901acc415c4a5743c3647378
Curt Grimes <github@curtgrimes.com>
2020-01-16 00:16:13 -0600

    Use hex-to-RGB library

--- app/mixins/hexToRGB.js
+++ app/mixins/hexToRGB.js
@@ -1,18 +1,10 @@
-export default {
-    methods: {
-        hexToRGB(hex) {
-            // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-            var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-            hex = hex.replace(shorthandRegex, function (m, r, g, b) {
-                return r + r + g + g + b + b;
-            });
+import hexRGB from 'hex-rgb';
 
-            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-            return result ? {
-                r: parseInt(result[1], 16),
-                g: parseInt(result[2], 16),
-                b: parseInt(result[3], 16)
-            } : null;
-        },
+export default {
+  methods: {
+    hexToRGB(hex) {
+      const { red: r, green: g, blue: b } = hexRGB(hex);
+      return { r, g, b };
     },
-}
+  },
+};
